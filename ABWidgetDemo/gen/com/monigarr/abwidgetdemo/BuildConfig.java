c8d9307791b80a82a834b93a96dc25c66b43956d
MONICA PETERS <monigarr@yahoo.com>
2013-06-17 21:52:56 -0400

    cleanup

--- ABWidgetDemo/gen/com/monigarr/abwidgetdemo/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.monigarr.abwidgetdemo;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- AudioGPSNetwork/gen/com/monigarr/audiogpsnetworkstatus/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.monigarr.audiogpsnetworkstatus;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
